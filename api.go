f9e3fa154d3c8f078cda17350e72def0859f2984
Mathis Joffre <mariusjoffre@gmail.com>
2023-06-03 17:20:23 +0200

    Use json to return a more complex eval response.
    Signed-off-by: Joffref <mariusjoffre@gmail.com>

--- api.go
+++ api.go
@@ -5,17 +5,19 @@ import "github.com/tetratelabs/wazero/api"
 type function string
 
 const (
-	allocate     function = "allocate"
-	deallocate   function = "deallocate"
-	setEntities  function = "set_entities"
-	setPolicies  function = "set_policies"
-	isAuthorized function = "is_authorized"
+	allocate           function = "allocate"
+	deallocate         function = "deallocate"
+	setEntities        function = "set_entities"
+	setPolicies        function = "set_policies"
+	isAuthorizedString function = "is_authorized_string"
+	isAuthorizedJSON   function = "is_authorized_json"
 )
 
 // exportFuncs returns a map of exported functions from the wasm module.
 func exportFuncs(module api.Module) map[string]api.Function {
 	exportedFuncs := make(map[string]api.Function)
-	exportedFuncs[string(isAuthorized)] = module.ExportedFunction(string(isAuthorized))
+	exportedFuncs[string(isAuthorizedString)] = module.ExportedFunction(string(isAuthorizedString))
+	exportedFuncs[string(isAuthorizedJSON)] = module.ExportedFunction(string(isAuthorizedJSON))
 	exportedFuncs[string(setEntities)] = module.ExportedFunction(string(setEntities))
 	exportedFuncs[string(setPolicies)] = module.ExportedFunction(string(setPolicies))
 	// allocate and deallocate help us manage memory in the wasm module.
